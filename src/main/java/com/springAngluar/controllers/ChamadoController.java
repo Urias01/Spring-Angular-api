1dc1539046aadcbad94be4c2726d7a61eb82a110
Urias01 <uriasluis64@gmail.com>
2022-09-12 16:45:11 -0300

    FindAll Chamados

--- src/main/java/com/springAngluar/controllers/ChamadoController.java
+++ src/main/java/com/springAngluar/controllers/ChamadoController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoController {
@@ -22,5 +25,13 @@ public class ChamadoController {
         Chamado obj = service.findById(id);
         return ResponseEntity.ok().body(new ChamadoDto(obj));
     }
-    
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDto>> findAll(){
+        List<Chamado> list = service.findAll();
+        List<ChamadoDto> listDto = list.stream().map(obj ->
+                new ChamadoDto(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
 }
