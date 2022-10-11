e9c6030c78033a07971217839f75229045a7cc66
Urias01 <uriasluis64@gmail.com>
2022-10-11 18:47:28 -0300

    corrige nome authorization

--- src/main/java/com/springAngluar/security/JWTAuthenticationFilter.java
+++ src/main/java/com/springAngluar/security/JWTAuthenticationFilter.java
@@ -57,8 +57,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String username = ((UserSS) authResult.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         response.setHeader("access-control-expose-headers",
-                "Autorization");
-        response.setHeader("Autorization",
+                "Authorization");
+        response.setHeader("Authorization",
                 "Bearer " + token);
     }
 
